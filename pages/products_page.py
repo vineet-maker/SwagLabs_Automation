472d8985296852b0aece9c20d3caf32857dcc838
Vineet Raj <NH3731@nslhub.in>
2026-01-12 16:43:06 +0530

    Finalize Selenium framework with product and checkout flow

--- pages/products_page.py
+++ pages/products_page.py
@@ -1,13 +1,36 @@
 from selenium.webdriver.common.by import By
-from utilities.base_class import BaseClass
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class ProductsPage(BaseClass):
 
-    ADD_TO_CART_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
-    CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
+class ProductsPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 15)
+
+        self.PAGE_TITLE = (By.CLASS_NAME, "title")
+        self.ADD_TO_CART_BTN = (By.ID, "add-to-cart-sauce-labs-backpack")
+        self.CART_BADGE = (By.CLASS_NAME, "shopping_cart_badge")
+        self.CART_ICON = (By.CLASS_NAME, "shopping_cart_link")
+
+    def get_title(self):
+        return self.wait.until(
+            EC.visibility_of_element_located(self.PAGE_TITLE)
+        ).text
 
     def add_product_to_cart(self):
-        self.wait_for_element(self.ADD_TO_CART_BACKPACK).click()
+        self.wait.until(
+            EC.element_to_be_clickable(self.ADD_TO_CART_BTN)
+        ).click()
+
+    def get_cart_count(self):
+        self.wait.until(
+            EC.text_to_be_present_in_element(self.CART_BADGE, "1")
+        )
+        return self.driver.find_element(*self.CART_BADGE).text
 
     def go_to_cart(self):
-        self.wait_for_element(self.CART_ICON).click()
+        self.wait.until(
+            EC.element_to_be_clickable(self.CART_ICON)
+        ).click()
