395ecf91cbc13f700556fdcc440941a7fefb7335
Vineet Raj <NH3731@nslhub.in>
2026-01-12 17:26:02 +0530

    Fix ChromeDriver mismatch using Selenium Manager

--- conftest.py
+++ conftest.py
@@ -1,9 +1,7 @@
 import pytest
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
@@ -11,12 +9,12 @@ def setup():
     chrome_options = Options()
 
     if os.getenv("CI"):
-        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
+
     yield driver
     driver.quit()
